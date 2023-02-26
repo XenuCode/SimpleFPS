7ccf7da0e6030b7884236b8245ddf31c1649b59c
xenu <xenucode@gmail.com>
2023-02-26 23:47:22 +0100

    Yoho

--- Assets/Pakiet broń/Strzelanie.cs
+++ Assets/Pakiet broń/Strzelanie.cs
@@ -1,6 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.Polybrush;
 
 public class Strzelanie : MonoBehaviour
 {
@@ -8,14 +10,31 @@ public class Strzelanie : MonoBehaviour
     public GameObject bulletPrefab;
     public float bulletSpeed = 10;
     public Animation animation;
-
+    public int ammoCount =30;
+    public int ammoInMagazine =30;
+    public TextMeshProUGUI magazine, totalAmmo;
+    private double lastTime = 0;
+    public float shootInterval =0.3f;
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Mouse0))
+        if (Input.GetKey(KeyCode.Mouse0)&& Time.timeAsDouble-lastTime > shootInterval && ammoInMagazine >0)
         {
+            ammoInMagazine--;
+            magazine.text = ammoInMagazine + "/30";
             var bullet = Instantiate(bulletPrefab, bulletSpawnPoint.position, bulletSpawnPoint.rotation);
             bullet.GetComponent<Rigidbody>().velocity = bulletSpawnPoint.forward * bulletSpeed;
+            lastTime = Time.timeAsDouble;
             //animation.Play("Shoot");
+        }
+
+        if (Input.GetKeyDown(KeyCode.R) && ammoCount + ammoInMagazine > 0)
+        {
+            animation.Play("Reload");
+            ammoCount += ammoInMagazine;
+            ammoInMagazine = Math.Clamp(ammoCount, 0, 30);
+            magazine.text = ammoInMagazine + "/30";
+            ammoCount -= Math.Clamp(ammoCount, 0, 30);
+            totalAmmo.text = ammoCount.ToString();
         }
     }
 }
