4b6ca962336f7184db93270f38d67a84e3914482
xenu <xenucode@gmail.com>
2023-02-26 13:16:49 +0100

    Moving Da ticktak

--- Assets/Scripts/TiktakScript.cs
+++ Assets/Scripts/TiktakScript.cs
@@ -1,10 +1,23 @@
+using System;
 using UnityEngine;
+using UnityEngine.AI;
 
 namespace DefaultNamespace
 {
     
     public class TiktakScript : UnityEngine.MonoBehaviour
     {
-        Transform transf;
+        private NavMeshAgent agent;
+        public Transform player;
+        private void Start()
+        {
+            agent = GetComponent<NavMeshAgent>();
+            agent.destination = player.position; 
+        }
+
+        private void FixedUpdate()
+        {
+            agent.destination = player.position;
+        }
     }
 }
