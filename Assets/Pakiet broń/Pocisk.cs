a60f32228cad3d0b4d073d29f7db536d1c4abb33
xenu <xenucode@gmail.com>
2023-03-03 22:35:27 +0100

    Added audio and some visuals

--- Assets/Pakiet broń/Pocisk.cs
+++ Assets/Pakiet broń/Pocisk.cs
@@ -7,6 +7,7 @@ public class Pocisk : MonoBehaviour
 {
     // Start is called before the first frame update
     public float life = 3;
+    [SerializeField] private AudioSource _audio;
     void Awake()
     {
         Destroy(gameObject, life);
@@ -14,6 +15,7 @@ public class Pocisk : MonoBehaviour
 
     void OnCollisionEnter(Collision enemy)
     {
+        _audio.Play();
         if (enemy.gameObject.CompareTag("Enemy"))
         {
             enemy.gameObject.GetComponent<TiktakScript>().Damage(40);
--- Assets/Pakiet broń/Strzelanie.cs
+++ Assets/Pakiet broń/Strzelanie.cs
@@ -1,8 +1,9 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
-using UnityEngine.Polybrush;
+using Math = UnityEngine.Polybrush.Math;
 
 public class Strzelanie : MonoBehaviour
 {
@@ -17,31 +18,50 @@ public class Strzelanie : MonoBehaviour
     private double lastTime = 0;
     public float shootInterval =0.3f;
     public float lightTime;
+    public Gradient ammoGradient;
+    public AudioSource shoot,reload;
+
+    private void Start()
+    {
+        magazine.color = ammoGradient.Evaluate( (float) ammoInMagazine / 30);
+        totalAmmo.color = ammoGradient.Evaluate( (float) ammoCount / 120);
+    }
+
     void Update()
     {
         if (Input.GetKey(KeyCode.Mouse0)&& Time.timeAsDouble-lastTime > shootInterval && ammoInMagazine >0)
         {
+            shoot.Play();
             ammoInMagazine--;
-            magazine.text = ammoInMagazine + "/30";
+            magazine.text = ammoInMagazine.ToString();
             var bullet = Instantiate(bulletPrefab, bulletSpawnPoint.position, bulletSpawnPoint.rotation);
             bullet.GetComponent<Rigidbody>().velocity = bulletSpawnPoint.forward * bulletSpeed;
             lastTime = Time.timeAsDouble;
             //animation.Play("Shoot");
             light.SetActive(true);
+            magazine.color = ammoGradient.Evaluate( (float) ammoInMagazine / 30);
+            totalAmmo.color = ammoGradient.Evaluate( (float) ammoCount / 240);
         }
 
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            animation.Play("AmmoInspect");
+        }
         if (Time.timeAsDouble-lastTime>lightTime)
         {
             light.SetActive(false);
         }
         if (Input.GetKeyDown(KeyCode.R) && ammoCount + ammoInMagazine > 0)
         {
+            reload.Play();
             animation.Play("Reload");
             ammoCount += ammoInMagazine;
             ammoInMagazine = Math.Clamp(ammoCount, 0, 30);
-            magazine.text = ammoInMagazine + "/30";
+            magazine.text = ammoInMagazine.ToString();
             ammoCount -= Math.Clamp(ammoCount, 0, 30);
             totalAmmo.text = ammoCount.ToString();
+            totalAmmo.color = ammoGradient.Evaluate( (float) ammoCount / 240);
+            magazine.color = ammoGradient.Evaluate( (float) ammoInMagazine / 30);
         }
     }
 }
